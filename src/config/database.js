5c7df95a4e85a272b6767b3bba84652c09f203ba
Lucas <lucas.santos@dedtechsolutions.com.br>
2020-01-27 15:05:06 -0300

    second commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgress',
+  host: 'localhost',
+  username: 'docker',
+  database: 'gobaber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
